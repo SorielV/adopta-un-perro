ce8acf8ef2ca2d4527bc1b469d62899e8ea538f0
francisco-j <ffuentes@ucol.mx>
2019-11-02 01:09:07 -0600

    mini routers

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.json()); //why? : https://stackoverflow.com/questions/47232187/express-json-vs-bodyparser-json
 
-app.use("/", require("./rutes/main")) //main domains
-app.use("/api", require("./rutes/api")) //api domains
+app.use("/perrera", require("./rutes/perrera")) //main domains
+app.use("/usuario", require("./rutes/usuario")) //main domains
+app.use("/api", require("./rutes/api.js")) //api domains
 
 require('./utilities/mongodb')(mongoose); //conect to mongoDB
 require('./utilities/pug')(app, express); //using pug
