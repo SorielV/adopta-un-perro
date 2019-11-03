c32f7b98db3a0e5786bdfdeabd9796bca1ba47c6
jorge <jfernandez4@ucol.mx>
2019-11-02 22:19:00 -0600

    cambios solo de front nada de back :C

--- rutes/perrera.js
+++ rutes/perrera.js
@@ -5,18 +5,6 @@ router.get("/", (req, res) => {
     res.render("test", { title: "asd" });
 });
 
-/*
-// post new drone
-router.post("/drone", (req, res) => {
-    //store {figth} in mongodb
-    droneDoc.save()
-        .catch((err) => {
-            res.status(500).send({ message: 'problem saving to the database' });
-            console.log(err);
-        }
-        )
-        .then((saved) => res.send(saved));
-});
-*/
+
 
 module.exports = router
