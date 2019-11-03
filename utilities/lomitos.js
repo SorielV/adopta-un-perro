06afa6a780e1eb6d43147114551a19aa17e813fe
bflores4cd <bflores4@ucol.mx>
2019-11-03 10:00:48 -0600

    merch con develop

--- utilities/lomitos.js
+++ utilities/lomitos.js
@@ -1,3 +1,5 @@
+import { functionTypeAnnotation } from "babel-types";
+
 function modalAdopt(){
     $('#myModal').css("margin-top", $(window).height() / 3.5 - $('.modal-content').height() / 3);
     $("#myModal").modal();
@@ -13,4 +15,33 @@ function solicitarAdoptar(){
 function solicitarDonar(){
     $("#verLomito").hide(); 
     $("#solicitudAdopt").show(); 
+}
+function añadirLomito(){
+    $("#newLomitoForm").modal();   
+}
+function verLomito() {//funcion que despliega modal con opciones a edityar y eliminar
+    $("#lomitoInformaton").modal();   
+ }
+
+function editLomito(){
+    $("#lomitoInformaton").hide();   
+    $("#editLomitoForm").modal(); 
+    
+ }
+ function guardarLomito(){
+
+ }
+ function añadirLomito(){
+    $("#newLomitoForm").modal();   
+}
+function añadirDogshome(){
+    $("#newdogshomeForm").modal();   
+    
+}
+function verDogshome() {//funcion que despliega modal con opciones a edityar y eliminar
+    $("#dogshomeinformation").modal();   
+ }
+
+function editDogsHome(){
+    $("#editDogshomeForm").modal();  
 }
